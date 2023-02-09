128513fc88a205c6628c01fc4f2eb012b2f92f81
Thanhtv1 <thanhtvt0311@gmail.com>
2023-02-09 15:25:31 +0700

    fixing connection

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const moongoose = require("mongoose");
+const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
@@ -13,7 +13,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connected to mongodb");
   } catch (error) {
     console.log(error);
